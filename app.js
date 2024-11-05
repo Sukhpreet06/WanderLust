4698c57d9ed977fc0320d44ea5c7e717dfaeb7f8
sukhpreet singh <sukhpreetsinghjii@gmail.com>
2024-11-05 12:56:37 +0530

    latest

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const  listingRouter =require("./routes/listing.js");
 const  reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const dburl= 'mongodb+srv://sukhpreetsinghjii:sukh123@cluster0.wnp48.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-const SECRET = mysupersecretcode;
+const SECRET = 'mysupersecretcode';
 
  async function main() {
     try {
